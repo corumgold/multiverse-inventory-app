21836dd60b3b039b00cbfe849d6b129e785a0178
Cory <goldcorum@gmail.com>
2023-01-26 12:32:11 -0500

    Add item model and express route for all items

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,21 @@
-const {Sequelize} = require('sequelize')
-const {sequelize} = require('../db')
+const { Sequelize } = require("sequelize");
+const { sequelize } = require("../db");
 
 const Sauce = sequelize.define("sauces", {
   name: Sequelize.STRING,
   image: Sequelize.STRING,
 });
 
+const Item = sequelize.define("items", {
+  name: Sequelize.STRING,
+  description: Sequelize.TEXT,
+  price: Sequelize.INTEGER,
+  category: Sequelize.STRING,
+  image: Sequelize.STRING,
+});
+
 module.exports = {
   db: sequelize,
   Sauce,
+  Item,
 };
