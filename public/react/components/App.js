324fd704cceb7ad707b0e5024a358ab5f0748825
Cory <goldcorum@gmail.com>
2023-01-26 12:44:36 -0500

    Display all items and sauces on main page

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,33 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
+import React, { useState, useEffect } from "react";
+import { SaucesList } from "./SaucesList";
 
 // import and prepend the api url to any fetch calls
-import apiURL from '../api';
+import apiURL from "../api";
+import { ItemsList } from "./ItemsList";
 
 export const App = () => {
+  const [sauces, setSauces] = useState([]);
+  const [items, setItems] = useState([]);
 
-	const [sauces, setSauces] = useState([]);
+  async function fetchSauces() {
+    try {
+      const response = await fetch(`${apiURL}/sauces`);
+      const saucesData = await response.json();
 
-	async function fetchSauces(){
-		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			
-			setSauces(saucesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+      setSauces(saucesData);
+    } catch (err) {
+      console.log("Oh no an error! ", err);
+    }
+  }
 
-	useEffect(() => {
-		fetchSauces();
-	}, []);
+  async function fetchItems() {
+    try {
+      const response = await fetch(`${apiURL}/items`);
+      const itemsData = await response.json();
+      setItems(itemsData);
+    } catch (err) {
+      console.log("Oh no an error! ", err);
+    }
+  }
 
-	return (
-		<main>	
+  useEffect(() => {
+    fetchSauces();
+    fetchItems();
+  }, []);
+
+  return (
+    <main>
       <h1>Sauce Store</h1>
-			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
-		</main>
-	)
-}
+      <h2>All things 🔥</h2>
+      <SaucesList sauces={sauces} />
+      <ItemsList items={items} />
+    </main>
+  );
+};
